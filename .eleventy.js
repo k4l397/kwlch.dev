983f23275e8692386108e39faa530cc4dceb2a8a
Kyle <k4l397@users.noreply.github.com>
2023-11-04 23:37:34 +0000

    add nice fonts and a pride flag

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = (config) => {
   config.addPassthroughCopy("CNAME");
   config.addPassthroughCopy("css");
   config.addPassthroughCopy("img");
+  config.addPassthroughCopy("fonts");
   return {
     dir: {
       input: "pages",
