bb59123aa7896f3ee77f61d9d06263b09f32cf00
Kyle <k4l397@users.noreply.github.com>
2023-11-04 22:50:36 +0000

    make the site prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = (config) => {
   config.addPassthroughCopy("CNAME");
+  config.addPassthroughCopy("css");
+  config.addPassthroughCopy("img");
   return {
     dir: {
       input: "pages",
