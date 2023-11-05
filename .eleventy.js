9dd273c981e34cdc19f5aa96f0299d63cda927b7
Kyle <k4l397@users.noreply.github.com>
2023-11-05 00:25:27 +0000

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = (config) => {
   config.addPassthroughCopy("css");
   config.addPassthroughCopy("img");
   config.addPassthroughCopy("fonts");
+  config.addPassthroughCopy("favicon.ico");
   return {
     dir: {
       input: "pages",
